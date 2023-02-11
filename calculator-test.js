ea4239e7839e8b497b8bf05e8b8b5f42227ae25c
Meaghan Sarno <meag@Meaghans-MacBook-Pro.local>
2023-02-11 18:14:34 -0500

    initial commit

---
+++ calculator-test.js
@@ -0,0 +1,25 @@
+
+it('should calculate the monthly rate correctly', function () {
+  // ...
+  const values = {
+
+  };
+  except(calculateMonthlyPayment(values)).toEqual();
+});
+
+
+it("should return a result with 2 decimal places", function() {
+  const values = {
+
+  }
+});
+
+
+it("should update the monthly payment", function () {
+  // ..
+});
+
+
+it("should update the UI", function () {
+  // ..
+});
